b15aaf5af56d951fd1a2f5a400e61b4b684514fa
Chad Banfield <cbanfiel@emich.edu>
2020-02-05 13:51:58 -0500

    messing around with firebase

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -5,22 +5,12 @@ import Management from '../scenes/Management';
 import Roster from '../scenes/Roster';
 import Login from '../scenes/Login';
 import LoadingScreen from '../scenes/LoadingScreen';
-
 import * as firebase from 'firebase';
 import Register from '../scenes/Register';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAHKokKwh7Feo7Ygx6judWV3V9jQ3VexHs",
-    authDomain: "coach-management-1422d.firebaseapp.com",
-    databaseURL: "https://coach-management-1422d.firebaseio.com",
-    projectId: "coach-management-1422d",
-    storageBucket: "coach-management-1422d.appspot.com",
-    messagingSenderId: "463745474336",
-    appId: "1:463745474336:web:85dc716a6c2d88df914145",
-    measurementId: "G-LYEHNQJBPM"
-  };
+const config = require('../config');
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config.firebase);
   // firebase.analytics();
 
 
